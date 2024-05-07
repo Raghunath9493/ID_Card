da171f58f4d790947faa0c82d691f238ed6ab197
Raghunath Kunigiri <54011917+Raghunath9493@users.noreply.github.com>
2024-05-07 10:40:39 -0500

    ID Card Generator

---
+++ ID_Generator.py
@@ -0,0 +1,75 @@
+import os
+import csv
+import tempfile
+from reportlab.platypus import SimpleDocTemplate, Image, PageBreak
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import landscape
+from PIL import Image as PILImage, ImageDraw, ImageFont
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.properties')
+
+id_template_path = config['Properties']['id_template']
+csv_file_path = config['Properties']['csv_file']
+output_filename = config['Properties']['output_filename']+".pdf"
+images_directory = config['Properties']['images_directory']
+
+temp_file_paths = []
+custom_page_width = 255.0
+custom_page_height = 165.0
+custom_page_size = landscape((custom_page_width, custom_page_height))
+
+
+
+def truncate_name(name, max_length):
+    """
+    Truncate the name if it exceeds the maximum length.
+    """
+    if len(name) > max_length:
+        return name[:max_length]
+    else:
+        return name
+
+
+
+def create_temporary_image(pil_image):
+    """
+    Create a temporary file from a PIL Image object and return its path.
+    """
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
+        pil_image.save(temp_file.name)
+        temp_file_path = temp_file.name
+        temp_file_paths.append(temp_file_path)
+    return temp_file_path
+
+doc = SimpleDocTemplate(output_filename, pagesize=custom_page_size, leftMargin=0, rightMargin=0, topMargin=0, bottomMargin=0)
+elements = []
+
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        template_image = PILImage.open(id_template_path)
+        name_font = ImageFont.truetype("arial.ttf", size=20)
+        title_font = ImageFont.truetype("arial.ttf", size=10)
+        name = truncate_name(row["Name"], 29)
+        title = row["Title"]
+        draw = ImageDraw.Draw(template_image)
+        draw.text((100, 170), name, font=name_font, fill=(0, 0, 0))
+        draw.text((200, 143), title, font=title_font, fill=(0, 0, 0))
+        photo_path = row["Location"]
+        photo_image = PILImage.open(os.path.join(images_directory, photo_path))
+        photo_image = photo_image.resize((100, 120))
+        template_image.paste(photo_image, (187, 20))
+        temp_file_path = create_temporary_image(template_image)
+        elements.append(Image(temp_file_path, 3.375 * inch, 2.125 * inch))
+        elements.append(PageBreak())
+
+doc.build(elements)
+
+for temp_file in temp_file_paths:
+    print("temp_file", temp_file)
+    os.remove(temp_file)
+
+print(f"ID cards have been generated in {output_filename}")
---
+++ ReadMe.md
@@ -0,0 +1,77 @@
+**ID CARD GENERATOR**
+
+This code can be used to generate ID Cards by using the data given in CSV file.
+
+The properties are given in config.properties file
+
+**config.properties file update**
+- **id_template** = Your id template location 
+- **csv_file** = THis file contains the data like Name, Position, Photo Location.
+- **output_filename** = The output file name with generated ID cards.
+- **images_directory** = The folder where the images are stored.
+
+**Commands to activate the venv:**
+
+# Python Virtual Environment Setup
+
+## macOS:
+
+1. **Create Virtual Environment:**
+   - Open Terminal.
+   - Navigate to your project directory.
+   - Run the command:
+     ```
+     python3 -m venv venv
+     ```
+
+2. **Activate Virtual Environment:**
+   - In the same Terminal window, activate the virtual environment by running:
+     ```
+     source venv/bin/activate
+     ```
+   - Your prompt will change to show the virtual environment name, indicating that it is now active.
+
+3. **Install Dependencies:**
+   - Ensure you have a `requirements.txt` file in your project directory.
+   - Install required Python packages by executing:
+     ```
+     pip install -r requirements.txt
+     ```
+
+## Windows:
+
+1. **Create Virtual Environment:**
+   - Open Command Prompt.
+   - Navigate to your project directory.
+   - Execute the command:
+     ```
+     py -m venv venv
+     ```
+
+2. **Activate Virtual Environment:**
+   - Still in Command Prompt, activate the virtual environment with:
+     ```
+     .\venv\Scripts\activate
+     ```
+   - You will see `(venv)` before your prompt, indicating the virtual environment is active.
+
+3. **Install Dependencies:**
+   - Make sure your project directory contains a `requirements.txt`.
+   - Install the necessary packages by running:
+     ```
+     pip install -r requirements.txt
+     ```
+
+## Notes:
+
+- Always activate the virtual environment before running your project scripts or installing new packages to ensure they are isolated from the global Python environment.
+- If you encounter any issues with package versions, consider upgrading `pip` within your virtual environment using `pip install --upgrade pip`.
+
+**Command to run the script:**
+- In macOS: <code>python3 ID_Generator.py</code>
+- In windows: <code>py ID_Generator.py</code>
+
+**Sample Output**
+
+<img width="1469" alt="image" src="C:\Users\kunig\OneDrive - Saint Louis University\Documents\SEM_2\PSD\Extra credit\ID-Card-Generator\Output.png">
+
